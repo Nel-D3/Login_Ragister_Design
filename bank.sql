53d1ba7c7ba9e9e0ac32b1b14904754c03270e63
Nel-D3 <59683300+Nel-D3@users.noreply.github.com>
2021-05-17 02:02:38 +0200

    voici la bonne base de données qui fait fonctionner le programme. Le registre refuse d'envoyer le nom dans la base de donnée le nom est à rentrer manuellement

--- bank.sql
+++ bank.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : Dim 16 mai 2021 à 22:58
+-- Généré le : Dim 16 mai 2021 à 23:53
 -- Version du serveur :  5.7.31
 -- Version de PHP : 7.3.21
 
@@ -24,29 +24,31 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Structure de la table `log`
+-- Structure de la table `compte`
 --
 
-DROP TABLE IF EXISTS `log`;
-CREATE TABLE IF NOT EXISTS `log` (
+DROP TABLE IF EXISTS `compte`;
+CREATE TABLE IF NOT EXISTS `compte` (
   `numcompte` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(50) NOT NULL,
-  `Nom` varchar(50) NOT NULL,
-  `Prénom` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
-  `numéro_tel` int(50) NOT NULL,
-  `solde` int(11) NOT NULL,
-  PRIMARY KEY (`numcompte`),
-  UNIQUE KEY `numéro_tel` (`numéro_tel`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+  `username` varchar(50) DEFAULT NULL,
+  `Nomprenom` varchar(50) DEFAULT NULL,
+  `password` varchar(50) DEFAULT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  `solde` double DEFAULT NULL,
+  `gender` varchar(20) DEFAULT NULL,
+  `picture` blob,
+  PRIMARY KEY (`numcompte`)
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
 --
--- Déchargement des données de la table `log`
+-- Déchargement des données de la table `compte`
 --
 
-INSERT INTO `log` (`numcompte`, `username`, `Nom`, `Prénom`, `password`, `numéro_tel`, `solde`) VALUES
-(1, 'NelD3', 'BOBEDZANG NTAWANGA', 'Nelson Bob-J', 'Twiligth721@', 625313976, 2000),
-(2, 'zero', 'bryan', 'nelson', '0714', 7143541, 4000);
+INSERT INTO `compte` (`numcompte`, `username`, `Nomprenom`, `password`, `phone`, `solde`, `gender`, `picture`) VALUES
+(1, 'NelD3', 'BOBEDZANG NTAWANGA', 'Twiligth721@', '0625313976', 2000, '0', NULL),
+(2, 'zero', 'bob', '0714', '07143541', 4000, '0', NULL),
+(5, 'boy', NULL, '12345', '07143541', 1000, 'Male', NULL),
+(6, 'db', NULL, '1234', '07143541', NULL, 'Female', NULL);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
